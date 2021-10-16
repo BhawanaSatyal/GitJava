d697ef9065b265d2d2fc47636a3a58697ef53e3f
BhawanaSatyal <bhawanasatyal21@gmail.com>
2021-10-16 21:58:19 +0100

    Initial commit

---
+++ src/GitJava.java
@@ -0,0 +1,7 @@
+public class GitJava {
+    public static void main(String[] args) {
+        System.out.println("I love Git");
+        System.out.println("I love Git");
+        System.out.println("I love Git");
+    }
+}
